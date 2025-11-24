83d9064c2f4d83580ec85bdedf8e1964ae36a73f
Dimas Andriansyah <andryan.dimas@gmail.com>
2025-11-24 09:56:03 +0000

    Maksud gue headbar, navbar yang diatas dan 1 halaman tidak freeze. Saat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <Header />
       
       {/* Welcome Section & Category Nav */}
-      <div className="sticky top-0 bg-black z-10 p-4 md:p-6 pb-4">
+      <div className="p-4 md:p-6 pb-4">
         <div className="flex items-center justify-between mb-4">
           <div>
             <h1 className="text-2xl md:text-3xl font-bold text-white">
